24bb9d0a1178f74c6327e07597052ce68f32f94e
angela.felisilda@ftwfoundation.org <angela.felisilda@ftwfoundation.org>
2026-09-24 17:19:03 +0000

    create catalog and schemas

--- notebooks/00_setup.sql
+++ notebooks/00_setup.sql
@@ -1,1 +1,5 @@
+CREATE CATALOG IF NOT EXISTS nyc_mobility;
 
+CREATE SCHEMA IF NOT EXISTS nyc_mobility.bronze;
+CREATE SCHEMA IF NOT EXISTS nyc_mobility.silver;
+CREATE SCHEMA IF NOT EXISTS nyc_mobility.gold;
